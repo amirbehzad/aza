16a7f6ef4e31f9a0af8f45d873c69fc4bb49eccd
azaaza <azaaza@06639159-424e-0410-83ec-15aca81afb91>
2008-05-28 05:54:42 +0000

    Test

--- SocialHistory/SocialHistory.js
+++ SocialHistory/SocialHistory.js
@@ -123,14 +123,10 @@ var SocialHistory = function( moreSites ){
     iframe.doc.open();
     iframe.doc.close();
     
-    iframe.doc.alinkColor = "#0000FF";
-    iframe.doc.linkColor = "#0000FF";
-    iframe.doc.vlinkColor = "#800080";
+    iframe.doc.body.alinkColor = "#0000FF";
+    iframe.doc.body.linkColor = "#0000FF";
+    iframe.doc.body.vlinkColor = "#800080";
     
-    var style = iframe.doc.createElement( "style" );
-    style.innerHTML = "a:visited{color:#800080;}";
-    iframe.doc.childNodes[0].childNodes[0].appendChild( style );
-
     // Return the iframe: iframe.doc contains the iframe.
     return iframe;
   }  
